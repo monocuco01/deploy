4478a35789e26d3ed70ec134d2d2c371a96590b8
monocuco01 <hsolano267@gmail.com>
2023-06-24 23:14:58 -0500

    tierro

--- api/src/app.js
+++ api/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
+// const cookieParser = require("cookie-parser");
+// const bodyParser = require("body-parser");
+// const morgan = require("morgan");
 const routes = require("./routes/index.js");
 
 require("./db.js");
@@ -10,10 +10,10 @@ const server = express();
 
 server.name = "API";
 
-server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-server.use(bodyParser.json({ limit: "50mb" }));
-server.use(cookieParser());
-server.use(morgan("dev"));
+// server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+// server.use(bodyParser.json({ limit: "50mb" }));
+// server.use(cookieParser());
+// server.use(morgan("dev"));
 // server.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", "https://deploy-inyx.vercel.app/"); // update to match the domain you will make the request from
 //   res.header("Access-Control-Allow-Credentials", "true");
